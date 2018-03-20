a544bc47c2914cda2ddcebfbb02bd8aff5d55ce6
Marlon <marlonpempengco@gmail.com>
2018-03-20 17:27:27 +0000

    started doing large screen display design for the about page, forgot to start mobile first. Gonna focus on the mobile first design first before doing anything else with the large screen design, which I have now put into meida queries. Used more CSS Grid with grid-column-start, grid-row-start and grid-row-end

--- about.php
+++ about.php
@@ -5,8 +5,68 @@
 
 <?php require("header.php");?>
 
-
 About
+
+<section id="secondary-header"></section>
+
+
+<section id="about-info">
+
+
+<div id="interactive-panel">
+
+<div class="photo">
+
+<img>
+
+</div>
+
+<div class="button button1">
+
+
+
+</div>
+
+<div class="button button2">
+
+
+</div>
+
+<div class="button button3">
+
+
+</div>
+
+<div class="button button4">
+
+
+
+</div>
+
+
+</div>
+
+<div id="info-text-box">
+
+<p>I'm a front end developer aspiring to become a badman full stack.</p>
+
+
+<button>More</button>
+
+
+</div>
+
+
+</section>
+
+
+<?php require("footer.php");?>
+
+
+
+
+
+
 
 </body>
 
