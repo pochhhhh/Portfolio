de13fa90e4a375540c9654c6f2615d42c54284ed
Marlon <marlonpempengco@gmail.com>
2018-03-23 16:27:01 +0000

    styled out the projects page on desktop view, added a temporary hover feature. Currently struggling on how to add a fader layer similar to the one on the homepage, the fader class won't behave in the same way. I am trying to figure how to add project name and skills used when hovering above the project item

--- projects.php
+++ projects.php
@@ -7,20 +7,31 @@
 <?php require("header.php");?>
 
 
-<section id="projects-title-header">Projects</section>
+<section id="projects-title-header"><h1>&#60;&sol;&gt;Projects</h1></section>
 
 
 <section id="project-list">
 
-<div class="project-item"><img src="https://preview.ibb.co/kTSw1w/Screen_Shot_2018_01_21_at_11_24_44.png"></div>
+<div class="project-item">
+<img src="https://preview.ibb.co/kTSw1w/Screen_Shot_2018_01_21_at_11_24_44.png">
+<div class="project-hover"></div>
+</div>
 
-<div class="project-item"><img src="https://preview.ibb.co/jw2tEG/biggytravels.png"></div>
+<div class="project-item">
+<img src="https://preview.ibb.co/jw2tEG/biggytravels.png">
+</div>
 
-<div class="project-item"><img src="https://preview.ibb.co/b7bpMw/Screen_Shot_2018_01_21_at_11_34_10.png"></div>
+<div class="project-item">
+<img src="https://preview.ibb.co/b7bpMw/Screen_Shot_2018_01_21_at_11_34_10.png">
+</div>
 
-<div class="project-item"><img src="https://preview.ibb.co/iGdQuG/Screen_Shot_2018_01_21_at_11_41_27.png"></div>
+<div class="project-item">
+<img src="https://preview.ibb.co/iGdQuG/Screen_Shot_2018_01_21_at_11_41_27.png">
+</div>
 
-<div class="project-item"><img src="https://image.ibb.co/ekkEkS/portfolio.png"></div>
+<div class="project-item">
+<img src="https://image.ibb.co/ekkEkS/portfolio.png">
+</div>
 
 
 </section>
