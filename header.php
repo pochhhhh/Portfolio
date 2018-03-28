360d605a6527c28b4b8ad002fe1042f4c4073752
Marlon <marlonpempengco@gmail.com>
2018-03-28 16:03:00 +0100

    started styling out the mobile version projects list. Spent the whole day coming up with a hover mechanism with darkerner on codepen which involves the use of jQuery and css property margin-top. I'm gonna style the rest of the mobile projects list and try to correctly position the hover layer so it fits well on mobile display

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <title>Marlon Pempengco</title>
 <link rel="stylesheet" href="style.css">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Handlee|Timmana" rel="stylesheet">
 <script
--- projects.php
+++ projects.php
@@ -12,26 +12,47 @@
 
 <section id="project-list">
 
-<div class="project-item">
-<img src="https://preview.ibb.co/kTSw1w/Screen_Shot_2018_01_21_at_11_24_44.png">
-<div class="project-hover"></div>
+<div id="project-a" class="box">
+    
+    <div class="project-image" style="background-image: url('https://preview.ibb.co/kTSw1w/Screen_Shot_2018_01_21_at_11_24_44.png');"></div>
+	<div class="hover-layer">
+    <div class="project-name"><h1>Simon Game</h1></div>
+      <div class="technologies">
+<i class="fab fa-js fa-2x"></i>
 </div>
+    </div>
+    
+  </div>
+  
+    <div id="project-b" class="box">
+    
+    <div class="project-image" style="background-image: url('https://preview.ibb.co/jw2tEG/biggytravels.png');"></div>
+    <div class="hover-layer">
+    <div class="project-name"><h1>BiggyTravels</h1></div>
+    <div class="technologies"><i class="fab fa-wordpress-simple fa-2x"></i><i class="fab fa-php fa-2x"></i><i class="fab fa-css3-alt fa-2x"></i>
 
-<div class="project-item">
-<img src="https://preview.ibb.co/jw2tEG/biggytravels.png">
-</div>
 
-<div class="project-item">
-<img src="https://preview.ibb.co/b7bpMw/Screen_Shot_2018_01_21_at_11_34_10.png">
-</div>
 
-<div class="project-item">
-<img src="https://preview.ibb.co/iGdQuG/Screen_Shot_2018_01_21_at_11_41_27.png">
-</div>
 
-<div class="project-item">
-<img src="https://image.ibb.co/ekkEkS/portfolio.png">
+
 </div>
+    </div>
+    
+  </div>
+  
+  
+    <div id="project-c" class="box">
+    
+    <div class="project-image" style="background-image: url('https://preview.ibb.co/iGdQuG/Screen_Shot_2018_01_21_at_11_41_27.png');"></div>
+    <div class="hover-layer">
+    <div class="project-name"><h1>JavaScript Calculator</h1></div>
+    <div class="technologies"><i class="fab fa-js fa-2x"></i>
+
+</div>
+    </div>
+    
+  </div>
+
 
 
 </section>
