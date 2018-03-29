05ea3d421e46c9054c9d6f1003f6414f99ab4a11
Marlon <marlonpempengco@gmail.com>
2018-03-29 12:00:14 +0100

    Added content and styling for the contact page.

--- contact.php
+++ contact.php
@@ -5,7 +5,36 @@
 
 <?php require("header.php");?>
 
+<section id="contact-title-header">
+
 Contact
+
+
+</section>
+
+<section id="contact-form">
+
+<div class="contact-image"></div>
+
+<form>
+
+<div class="form-header"><p>Have something to share? Please do – I’d love to hear it!</p></div>
+
+<input type="text" name="name" placeholder="Name"></input>
+
+<input type="text" name="email" placeholder="Email"></input>
+
+<textarea name="message" placeholder="Message"></textarea>
+
+<button type="submit" value="Submit">Send</button>
+
+
+</form>
+
+
+</section>
+
+<?php require("footer.php");?>
 
 </body>
 
