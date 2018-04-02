9c4516a2fca8cd4c50d063478fcc167dd9fa93ae
Marlon <marlonpempengco@gmail.com>
2018-04-02 09:57:35 +0100

    Added contact form functionality. Set up email validation system. Next I will style alert messages based on outcome of contact form if statements

--- contact.php
+++ contact.php
@@ -1,3 +1,47 @@
+<?php
+
+$name = "";
+$email = "";
+$message = "";
+$subject = "Someone has viewed your portfolio and sent you a message!";
+
+
+if(filter_has_var(INPUT_POST, "submit")){
+
+if(!empty($_POST["name"]) && !empty($_POST["message"]) && !empty($_POST["email"])){
+
+if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$emailOutput = "
+
+From: $name
+Email Address: $email
+$message";
+
+echo "Thank you for message!";
+echo $emailOutput;
+mail("marlonpempengco@gmail.com", $subject, $emailOutput);
+
+
+}
+
+} else {
+
+echo "Please fill in all fields";
+
+}
+
+
+
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -16,7 +60,7 @@
 
 <div class="contact-image"></div>
 
-<form>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
 <div class="form-header"><p>Have something to share? Please do – I’d love to hear it!</p></div>
 
@@ -26,7 +70,7 @@
 
 <textarea name="message" placeholder="Message"></textarea>
 
-<button type="submit" value="Submit">Send</button>
+<button type="submit" name="submit">Send</button>
 
 
 </form>
