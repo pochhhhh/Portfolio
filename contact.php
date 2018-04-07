9103d517d2d31e1f870947b886298568f759b829
Marlon <marlonpempengco@gmail.com>
2018-04-07 18:09:46 +0100

    Made the contact form more robust by adding several conditions and changing the css borders of each input depending on the condition

--- contact.php
+++ contact.php
@@ -4,7 +4,15 @@
 $subject = "Someone has viewed your portfolio and sent you a message!";
 $messageBackground = "";
 $formHeaderMessage = "If you would like to work with me or have any enquries please feel free to fill out the form below!";
-$borderColor = "";
+$nameBorder = "";
+$emailBorder = "";
+$messageBorder = "";
+$redBorder = "3px solid #ef5353";
+$redBackground = "3px solid #499944";
+$greenBackground = "#5abc5c";
+$greenBorder = "3px solid #5abc5c";
+
+
 
 
 if(filter_has_var(INPUT_POST, "submit")){
@@ -27,13 +35,13 @@ if(!empty($name) && !empty($email) && !empty($message)){
 		//Failed email
 $messageBackground = "#ef5353";
 $formHeaderMessage = "Please validate email address";
-$borderColor = "3px solid #ef5353";
-
+$nameBorder = $greenBorder;
+$emailBorder = $redBorder;
+$messageBorder = $greenBorder;
 	} else {
 
 		//Passed email
-$messageBackground = "#5abc5c";
-$formHeaderMessage = "Thank you for your message!";
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
@@ -43,15 +51,97 @@ Email Address: $email
 $message";
 mail("marlonpempengco@gmail.com", $subject, $emailOutput);
 
+//Check if email was sent successfully
+
+if(mail("marlonpempengco@gmail.com", $subject, $emailOutput)){
+
+	$messageBackground = "#5abc5c";
+	$formHeaderMessage = "Thank you for your message!";
+//Thank you for your message!
+
+} else{
+
+//Message not sent :($messageBackground = "#5abc5c";
+$formHeaderMessage = "Message was not sent :(";
+	$messageBackground = "red";
+
+}
+
 	}
 
 } else {
 
 	//Failed
-	//Please fill in all fields
 
 $messageBackground = "#ef5353";
+$nameBorder = $redBorder;
+$emailBorder = $redBorder;
+$messageBorder = $redBorder;
+
 $formHeaderMessage = "Please fill in all fields";
+
+if(!empty($name) && empty($email) && empty($message)){
+
+$messageBackground = "#ef5353";
+
+	$nameBorder = $greenBorder;
+	$emailBorder = $redBorder;
+	$messageBorder = $redBorder;
+
+	$formHeaderMessage = "Please fill in all fields";
+
+}
+
+if(!empty($name) && !empty($email) && empty($message)){
+
+$messageBackground = "#ef5353";
+
+	$nameBorder = $greenBorder;
+	$emailBorder = $redBorder;
+	$messageBorder = $redBorder;
+
+	$formHeaderMessage = "Please fill in all fields";
+
+}
+
+if(!empty($name) && empty($email) && !empty($message)){
+
+$messageBackground = "#ef5353";
+
+	$nameBorder = $greenBorder;
+	$emailBorder = $redBorder;
+	$messageBorder = $greenBorder;
+
+	$formHeaderMessage = "Please fill in all fields";
+
+}
+
+if(empty($name) && !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($message)){
+
+$messageBackground = "#ef5353";
+
+	$nameBorder = $redBorder;
+	$emailBorder = $greenBorder;
+	$messageBorder = $greenBorder;
+
+	$formHeaderMessage = "Please fill in all fields";
+
+}
+
+if(empty($name) && !empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($message)){
+
+$messageBackground = "#ef5353";
+
+	$nameBorder = $redBorder;
+	$emailBorder = $redBorder;
+	$messageBorder = $greenBorder;
+
+	$formHeaderMessage = "Please fill in all fields";
+
+}
+
+
+
 
 
 
@@ -85,11 +175,11 @@ $formHeaderMessage = "Please fill in all fields";
 
 <div style="background: <?php echo $messageBackground;?>" class="form-header"><p id="form-header-text"><?php echo $formHeaderMessage; ?></p></div>
 
-<input type="text" name="name" placeholder="Name"></input>
+<input type="text" name="name" placeholder="Name" style="border: <?php echo $nameBorder;?>" value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>"></input>
 
-<input type="text" name="email" placeholder="Email" style="border: <?php echo $borderColor;?>"></input>
+<input type="text" name="email" placeholder="Email" style="border: <?php echo $emailBorder;?>" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>"></input>
 
-<textarea name="message" placeholder="Message"></textarea>
+<textarea name="message" placeholder="Message" style="border: <?php echo $messageBorder;?>"><?php echo isset($_POST['message']) ? $_POST['message'] : '' ?></textarea>
 
 <button type="submit" name="submit"><p>Send</p></button>
 
