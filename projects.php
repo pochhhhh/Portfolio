e4ded988aa6e01cf953e19bf347cd87aabb8e57a
Marlon <marlonpempengco@gmail.com>
2018-04-07 16:52:03 +0100

    added more media queries to projects page to make technologies panel more consistent when changing window display sizse

--- projects.php
+++ projects.php
@@ -16,7 +16,7 @@
 
     <div class="project-image" style="background-image: url('https://preview.ibb.co/kTSw1w/Screen_Shot_2018_01_21_at_11_24_44.png');"></div>
     <div class="hover-layer hidden">
-    <div class="project-name"><h1>Simon Game</h1><i class="project-github-logo fa fa-github fa-4x"></i></div>
+    <div class="project-name"><h1>Simon Game</h1><!--<i class="project-github-logo fa fa-github fa-4x"></i>--></div>
     <div class="technologies">
 <i class="fab fa-js fa-4x"></i>
 </div>
@@ -28,7 +28,7 @@
 
     <div class="project-image" style="background-image: url('https://preview.ibb.co/jw2tEG/biggytravels.png');"></div>
     <div class="hover-layer hidden">
-  <div class="project-name"><h1>BiggyTravels</h1><i class="project-github-logo fa fa-github fa-4x"></i></div>
+  <div class="project-name"><h1>BiggyTravels</h1><!--<i class="project-github-logo fa fa-github fa-4x"></i>--></div>
     <div class="technologies"><i class="fab fa-wordpress-simple fa-4x"></i><i class="fab fa-php fa-4x"></i><i class="fab fa-css3-alt fa-4x"></i>
 
 
@@ -43,8 +43,8 @@
 
     <div class="project-image" style="background-image: url('https://preview.ibb.co/iGdQuG/Screen_Shot_2018_01_21_at_11_41_27.png');"></div>
     <div class="hover-layer hidden">
-    <div class="project-name"><h1>JavaScript Calculator</h1><i class="project-github-logo fa fa-github fa-4x"></i></div>
-    <div class="technologies" style="position:relative; top:-200px !important;"><i class="fab fa-js fa-4x"></i>
+    <div class="project-name"><h1>JavaScript Calculator</h1><!--<i class="project-github-logo fa fa-github fa-4x"></i>--></div>
+    <div class="technologies" style="position:relative; top:-220px !important;"><i class="fab fa-js fa-4x"></i>
 
 </div>
 </div>
@@ -54,8 +54,8 @@
 
   <div class="project-image" style="background-image: url('https://preview.ibb.co/b7bpMw/Screen_Shot_2018_01_21_at_11_34_10.png');"></div>
   <div class="hover-layer hidden">
-  <div class="project-name"><h1>Tic Tac Toe Game</h1><i class="project-github-logo fa fa-github fa-4x"></i></div>
-  <div class="technologies" style="position:relative; top:-200px !important;"><i class="fab fa-js fa-4x"></i>
+  <div class="project-name"><h1>Tic Tac Toe Game</h1><!--<i class="project-github-logo fa fa-github fa-4x"></i>--></div>
+  <div class="technologies" style="position:relative; top:-220px !important;"><i class="fab fa-js fa-4x"></i>
 
 </div>
   </div>
@@ -66,7 +66,7 @@
 
 <div class="project-image" style="background-image: url('images/portfolio-project.png');"></div>
 <div class="hover-layer hidden">
-<div class="project-name"><h1>Portfolio Site</h1><i class="project-github-logo fa fa-github fa-4x"></i></div>
+<div class="project-name"><h1>Portfolio Site</h1><!--<i class="project-github-logo fa fa-github fa-4x"></i>--></div>
 <div class="technologies">
   <i class="fab fa-sass fa-4x"></i>
 <i class="fab fa-js fa-4x"></i>
